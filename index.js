ee69a81a7c1eedbefbacff62c74667024fd239df
0xSohtan <safinmiah1306@gmail.com>
2023-08-06 20:13:39 +0200

    Add FAQ, Kontakt Page, Impressum Page

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+console.log("Made by Sohtan");
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        answer.classList.toggle('show');
+    });
+});
